5ededee7e690fc5028f29345de33e44f16a48435
Saiban Hussein <shhussein@m.marywood.edu>
2021-11-18 19:01:53 -0600

    wire code in main file

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  /* Show the search history from localStorage on first page load */
+  displaySearchHistory();
+  
+  /* Event listener for submitting search form directly */
+  $('#search-form').on('submit', function(event) {
+      event.preventDefault();
+      let searchTerm = $('#search-field').val();
+      runSearch(searchTerm);
+  });
+
+  /* Event listener for clicking on search button */
+  $('#search-button').on('click', function(event) {
+      let searchTerm = $('#search-field').val();
+      runSearch(searchTerm);
+  });
+
+  /* Helper function to run the search */
+  const runSearch = (searchTerm) => {
+      mealSearch(searchTerm);
+  };
+});
