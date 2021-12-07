b41c4f54e35d216e1a3b3365c859f8915bf1082e
Saiban Hussein <shhussein@m.marywood.edu>
2021-12-07 04:17:01 -0600

    in main javascript file displaySearchHistory funtion is to  Show the search history from localStorage on first page load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   /* Show the search history from localStorage on first page load */
   displaySearchHistory();
-  
+    
   /* Event listener for submitting search form directly */
   $('#search-form').on('submit', function(event) {
       event.preventDefault();
